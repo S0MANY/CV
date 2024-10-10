aec5d722703767f1d5f53fe4146a4bf0f924fb58
SOMANY <gena.popov.99@mail.ru>
2024-10-11 00:18:24 +0300

    tg added

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -14,31 +14,31 @@ function Experience(props) {
                             <li>Java Script</li>
                             <li>Swiper</li>
                         </ul>
-                        <BtnLink text={"Перейти на сайт"} link={"https://s0many.github.io/Catalog_element/"} size={"s"} theme={props.theme}/>
+                        <BtnLink text={"Перейти на сайт"} link={"https://s0many.github.io/Catalog-elem/"} size={"s"} theme={props.theme}/>
                         {/* <p className="link"><a href="https://s0many.github.io/Catalog_element/" target="_blank">Ссылка</a></p> */}
                     </li>
                     <li>
                         <p><strong>Витрина товаров</strong></p>
                         <p>Реализованы</p>
                         <ul>
-                            <li>Верстка ( Desktop only )</li>
+                            <li>Адаптивная верстка</li>
                             <li>React приложение</li>
                             <li>Работа с готовым API</li>
                         </ul>
-                        <BtnLink text={"Перейти на сайт"} link={"https://s0many.github.io/fortnite_store_app/"} size={"s"} theme={props.theme}/>
+                        <BtnLink text={"Перейти на сайт"} link={"https://s0many.github.io/fortnite_market/"} size={"s"} theme={props.theme}/>
                         {/* <p className="link"><a href="https://s0many.github.io/fortnite_store_app/" target="_blank">Ссылка</a></p> */}
                     </li>
                     <li>
                         <p><strong>Кулинарный сайт</strong></p>
                         <p>Реализованы</p>
                         <ul>
-                            <li>Верстка ( Desktop only )</li>
+                            <li>Адаптивная верстка</li>
                             <li>React приложение</li>
                             <li>Роутинг</li>
                             <li>Работа с готовым API</li>
                             <li>Swiper</li>
                         </ul>
-                        <BtnLink text={"Перейти на сайт"} link={"https://s0many.github.io/Cooking_react_app/"} size={"s"} theme={props.theme}/>
+                        <BtnLink text={"Перейти на сайт"} link={"https://s0many.github.io/Cooking_finder/"} size={"s"} theme={props.theme}/>
                         {/* <p className="link"><a href="https://s0many.github.io/Cooking_react_app/" target="_blank">Ссылка</a></p> */}
                     </li>
             
